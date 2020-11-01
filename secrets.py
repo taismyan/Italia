889aee55bf464267d34c4a2d8fae9f2d015b8551
taismyan <73716660+taismyan@users.noreply.github.com>
2020-11-01 15:05:14 -0600

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,8 +1,8 @@
-uuid = "9c0c642981bc08b42e18dcdf2738735f984059ec"                   # SHOULD match intercepted payload
-devicename = "iPhone5,3"    # SHOULD match intercepted payload
-devicewidth = 320           # SHOULD match intercepted payload
-deviceheight = 568          # SHOULD match intercepted payload
-token = "0a635337ff89a59ac7ff9763302ca906d9ea59c748c6549361c9902636a54f05"                  # SHOULD match intercepted payload
-os_version = "iOS 10.3.3"     # SHOULD match intercepted payload
+uuid = "3C51E3D2-ADCF-4FD8-B4F7-E2E4C8248CEF"                   # SHOULD match intercepted payload
+devicename = "iPhone10%2C2"    # SHOULD match intercepted payload
+devicewidth = 414           # SHOULD match intercepted payload
+deviceheight = 736          # SHOULD match intercepted payload
+token = "cc61ef23041beb8f621076b71c035776"                  # SHOULD match intercepted payload
+os_version = "iOS 14.1.1"     # SHOULD match intercepted payload
 # put some UA that is compatible with your device
 user_agent = "'Mozilla/5.0 (Apple-iPhone7C2/1202.466; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543 Safari/419.3'"
